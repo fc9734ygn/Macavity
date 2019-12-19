605ba2ea54c081d457cc72912b6216597d545a67
Mantas <latkauskas.mantas@gmail.com>
2019-12-19 19:55:11 +0000

    Added base activity, few packages

---
+++ app/src/main/java/com/example/macavity/ui/base/BaseActivity.kt
@@ -0,0 +1,6 @@
+package com.example.macavity.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity  : AppCompatActivity() {
+}
---
+++ app/src/main/java/com/example/macavity/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.macavity.ui
+
+import android.os.Bundle
+import com.example.macavity.R
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
