f0ffecb6fc026b9e5e1ecc41d6f0d719b574fc77
Mantas <latkauskas.mantas@gmail.com>
2019-12-23 15:34:53 +0000

    Implemented generic error message

--- app/src/main/java/com/example/macavity/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/macavity/ui/base/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.example.macavity.ui.base
 
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.example.macavity.R
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
@@ -8,4 +10,8 @@ open class BaseActivity : DaggerAppCompatActivity() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    fun showError(message: String = getString(R.string.default_error_message)){
+        Toast.makeText(this,message, Toast.LENGTH_LONG).show()
+    }
 }
