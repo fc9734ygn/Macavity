0d9b0dbbb84d8671b33e9c46e6eb6350f6bd2f36
Mantas <latkauskas.mantas@gmail.com>
2020-01-11 15:52:48 +0000

    added no internet dialog

--- app/src/main/java/com/example/macavity/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/macavity/ui/base/BaseActivity.kt
@@ -1,26 +1,49 @@
 package com.example.macavity.ui.base
 
+import am.appwise.components.ni.NoInternetDialog
 import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.macavity.R
 import dagger.android.support.DaggerAppCompatActivity
+import org.androidannotations.annotations.AfterViews
+import org.androidannotations.annotations.EActivity
 import javax.inject.Inject
+
 
 open class BaseActivity : DaggerAppCompatActivity() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    fun showError(message: String? = getString(R.string.default_error_message)) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    lateinit var noInternetDialog: NoInternetDialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initNoInternetDialog()
     }
 
-    fun goToActivity(activity: BaseActivity){
-        val intent = Intent(this, activity::class.java)
-        startActivity(intent)
+    override fun onDestroy() {
+        super.onDestroy()
+        noInternetDialog.onDestroy()
+    }
+
+    private fun initNoInternetDialog(){
+        noInternetDialog = NoInternetDialog.Builder(this)
+            .setBgGradientStart(getColor(R.color.blue_light))
+            .setBgGradientCenter(getColor(R.color.blue_primary))
+            .setBgGradientEnd(getColor(R.color.blue_dark))
+            .setButtonColor(getColor(R.color.amber_primary))
+            .build()
+
+    }
+
+    fun showError(message: String? = getString(R.string.default_error_message)) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
     private fun setFullScreen(){
