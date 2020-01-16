a7b53692da2853fc61f875799035aca88eb9f89a
Mantas <latkauskas.mantas@gmail.com>
2020-01-16 21:32:41 +0000

    Started with Edit Profile UI

--- app/src/main/java/com/example/macavity/ui/editProfile/EditProfileFragment.kt
+++ app/src/main/java/com/example/macavity/ui/editProfile/EditProfileFragment.kt
@@ -2,8 +2,11 @@ package com.example.macavity.ui.editProfile
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import com.example.macavity.R
 
 import com.example.macavity.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_edit_profile.*
+import org.androidannotations.annotations.AfterViews
 import org.androidannotations.annotations.EFragment
 
 @EFragment(resName = "fragment_edit_profile")
@@ -11,10 +14,21 @@ open class EditProfileFragment : BaseFragment() {
 
     private lateinit var vm: EditProfileViewModel
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    @AfterViews
+    fun afterViews() {
         vm = ViewModelProviders.of(this).get(EditProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+        initToolbar()
+        toggleBottomNav(false)
+    }
+
+    private fun initToolbar() {
+        toolbar.setTitle(getString(R.string.profile_edit_toolbar_title))
+            .setStartIcon(R.drawable.ic_arrow_back)
+            .setEndIcon(R.drawable.ic_save)
+
+        toolbar.startIconListener = { activity!!.onBackPressed() }
+        //TODO: save changes
+        toolbar.endIconListener = {}
     }
 
 }
--- app/src/main/java/com/example/macavity/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/macavity/ui/profile/ProfileFragment.kt
@@ -2,12 +2,14 @@ package com.example.macavity.ui.profile
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
 import com.example.macavity.R
 
 import com.example.macavity.ui.base.BaseFragment
 import com.example.macavity.ui.calendar.CalendarViewModel
 import kotlinx.android.synthetic.main.fragment_map.*
 import org.androidannotations.annotations.AfterViews
+import org.androidannotations.annotations.Click
 import org.androidannotations.annotations.EFragment
 
 @EFragment(resName = "fragment_profile")
@@ -23,8 +25,15 @@ open class ProfileFragment : BaseFragment() {
 
     private fun initToolbar() {
         toolbar.setStartIcon(R.drawable.ic_menu)
+            .setEndIcon(R.drawable.ic_edit)
             .setTitle(getString(R.string.toolbar_title_profile))
 
         toolbar.startIconListener = { openDrawer() }
+        //TODO: disable if not your profile
+        toolbar.endIconListener = { goToEditProfile() }
+    }
+
+    private fun goToEditProfile() {
+        findNavController().navigate(R.id.action_profileFragment__to_editProfileFragment_)
     }
 }
