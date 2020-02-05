91e120bee765ce8592aed5558c56ecfbd85d9e0c
Mantas <latkauskas.mantas@gmail.com>
2020-02-05 18:03:05 +0000

    Group fragment ui

--- app/src/main/java/com/example/macavity/ui/group/GroupFragment.kt
+++ app/src/main/java/com/example/macavity/ui/group/GroupFragment.kt
@@ -1,7 +1,12 @@
 package com.example.macavity.ui.group
 
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.macavity.R
+import com.example.macavity.data.models.Journey
+import com.example.macavity.data.models.User
 
 import com.example.macavity.ui.base.HomeFragment
 import kotlinx.android.synthetic.main.fragment_group.*
@@ -12,11 +17,28 @@ import org.androidannotations.annotations.EFragment
 open class GroupFragment : HomeFragment() {
 
     private lateinit var vm: GroupViewModel
+    //todo: pass user id
+    private val membersAdapter =
+        MembersAdapter { findNavController().navigate(R.id.action_groupFragment__to_profileFragment_) }
 
     @AfterViews
     fun afterViews() {
         vm = ViewModelProviders.of(this).get(GroupViewModel::class.java)
         initToolbar()
+        initRecyclerView()
+        //todo: use real data
+        group_stat.text = "56"
+    }
+
+    private fun initRecyclerView() {
+
+        members_rv.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        members_rv.adapter = membersAdapter
+
+        //TODO: user real data
+        membersAdapter.submitList(dummyData)
+
     }
 
     private fun initToolbar() {
@@ -25,4 +47,85 @@ open class GroupFragment : HomeFragment() {
 
         toolbar.startIconListener = { openDrawer() }
     }
+
+    //TODO: use real data
+    private val dummyData = listOf(
+        User(
+            "123",
+            "John",
+            "Main Street 25",
+            "Knight Ave 87",
+            Pair(12.2, 51.2),
+            Pair(21.2, 13.2),
+            "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            "john.smith@gmail.com",
+            "+44123123123",
+            true,
+            "AH3K24",
+            4,
+            41,
+            23
+        ), User(
+            "123",
+            "Veronica",
+            "Main Street 25",
+            "Knight Ave 87",
+            Pair(12.2, 51.2),
+            Pair(21.2, 13.2),
+            "https://s3.envato.com/files/236559784/preview.jpg",
+            "john.smith@gmail.com",
+            "+44123123123",
+            true,
+            "AH3K24",
+            4,
+            7,
+            3
+        ), User(
+            "123",
+            "Erick",
+            "Main Street 25",
+            "Knight Ave 87",
+            Pair(12.2, 51.2),
+            Pair(21.2, 13.2),
+            "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            "john.smith@gmail.com",
+            "+44123123123",
+            true,
+            "AH3K24",
+            4,
+            88,
+            23
+        ), User(
+            "123",
+            "Rachel",
+            "Main Street 25",
+            "Knight Ave 87",
+            Pair(12.2, 51.2),
+            Pair(21.2, 13.2),
+            "https://media.istockphoto.com/photos/young-longhaired-smiling-woman-in-white-shirt-picture-id965523228?k=6&m=965523228&s=612x612&w=0&h=qeVmQfjRq1QWxaLdxLdF_IaXahI-dqt9UYcunaHUqA4=",
+            "john.smith@gmail.com",
+            "+44123123123",
+            true,
+            "AH3K24",
+            4,
+            2,
+            73
+        ),
+        User(
+            "123",
+            "Olivia",
+            "Main Street 25",
+            "Knight Ave 87",
+            Pair(12.2, 51.2),
+            Pair(21.2, 13.2),
+            "https://www.maybelline.com/~/media/mny/us/face-makeup/modules/pathing-switcher/face-category-pathing-switcher.jpg",
+            "john.smith@gmail.com",
+            "+44123123123",
+            true,
+            "AH3K24",
+            4,
+            20,
+            0
+        )
+    )
 }
---
+++ app/src/main/java/com/example/macavity/ui/group/MembersAdapter.kt
@@ -0,0 +1,71 @@
+package com.example.macavity.ui.group
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.macavity.R
+import com.example.macavity.data.models.User
+import kotlinx.android.synthetic.main.view_member.view.*
+
+class MembersAdapter(private val itemClickListener: (User) -> Unit = {}) :
+    ListAdapter<User, RecyclerView.ViewHolder>(ListItemCallback()) {
+
+    class ListItemCallback : DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.passengerStat == newItem.passengerStat
+                    && oldItem.driverStat == newItem.driverStat
+                    && oldItem.avatarUrl == newItem.avatarUrl
+                    && oldItem.email == newItem.email
+                    && oldItem.phoneNumber == newItem.phoneNumber
+                    && oldItem.isDriver == newItem.isDriver
+                    && oldItem.name == newItem.name
+                    && oldItem.locationAddress == newItem.locationAddress
+                    && oldItem.destinationAddress == newItem.destinationAddress
+                    && oldItem.locationCoordinate == newItem.locationCoordinate
+                    && oldItem.destinationCoordinate == newItem.destinationCoordinate
+                    && oldItem.carNumberPlate == newItem.carNumberPlate
+                    && oldItem.carFreeSeats == newItem.carFreeSeats
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.view_member, parent, false)
+        return MemberItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = getItem(position)
+        (holder as MemberItemViewHolder).bind(item, position)
+    }
+
+    inner class MemberItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(member: User, position: Int) {
+
+            itemView.member_name.text = member.name
+            itemView.member_driver_stat.text = String.format(itemView.context.getString(R.string.journey_details_driver_stat), member.driverStat)
+            itemView.member_passenger_stat.text = String.format(itemView.context.getString(R.string.journey_details_passenger_stat), member.passengerStat)
+            setAvatarImage(member.avatarUrl)
+            itemView.setOnClickListener {
+                itemClickListener.invoke(member)
+            }
+        }
+
+        private fun setAvatarImage(url: String) {
+            Glide.with(itemView)
+                .load(url)
+                .circleCrop()
+                .placeholder(R.drawable.ic_person)
+                .error(R.drawable.ic_person)
+                .into(itemView.member_avatar)
+        }
+    }
+}
